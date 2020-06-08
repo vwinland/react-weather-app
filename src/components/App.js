c16ceb76b3c1a4e2ec979e76b600e06dad303cd3
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-06-07 20:19:46 -0400

    adds webpack and gets rid of styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  return <div>Test</div>;
-};
-console.log("process.env.REACT_APP_API_KEY");
-export default App;
+export default class App extends Component {
+  render() {
+    return <div>Bjarne Strousstroup</div>;
+  }
+}
